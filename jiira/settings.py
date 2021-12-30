6fd28baeedad4d61c7357f31815a6cf60351433a
johnroutledge <john_routledge@hotmail.com>
2021-12-30 18:20:51 +0000

    deployment to Heroku

--- jiira/settings.py
+++ jiira/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d1arcq&scho8zm3w-=g8^4y6hh&pi$sypufmexlt^v3gs%qx-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jiira.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,18 @@ WSGI_APPLICATION = 'jiira.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Connect to Postgres db if running on Heroku, otherwise connect to SQL Lite
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
