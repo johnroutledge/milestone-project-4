b6ad329e0df081fb9fa2180cad77b04c08de416b
johnroutledge <john_routledge@hotmail.com>
2021-12-30 20:37:58 +0000

    remove secret key and debug

--- jiira/settings.py
+++ jiira/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d1arcq&scho8zm3w-=g8^4y6hh&pi$sypufmexlt^v3gs%qx-t'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['jiira.herokuapp.com', 'localhost']
 
