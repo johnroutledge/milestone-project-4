cc270194cc237f98542050e34136df47c193ed62
johnroutledge <john_routledge@hotmail.com>
2022-01-09 23:26:10 +0000

    homepage, basket and toast styling

--- static/js/base.js
+++ static/js/base.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    //change the integers below to match the height of your upper div, which I called
-    //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
-    //to figure out what the scroll position is when exactly you want to fix the nav
-    //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
-    //you know the position.
-    $(window).scroll(function () {
-      if ($(window).scrollTop() > 30) {
-        $('#nav-bar').addClass('fixed-top');
-      }
-  
-      if ($(window).scrollTop() < 31) {
-        $('#nav-bar').removeClass('fixed-top');
-      }
-    });
+  //change the integers below to match the height of your upper div, which I called
+  //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+  //to figure out what the scroll position is when exactly you want to fix the nav
+  //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+  //you know the position.
+  $(window).scroll(function () { 
+    if ($(window).scrollTop() > 30) {
+      $('#topnav').addClass('navbar-fixed-top');
+    }
+
+    if ($(window).scrollTop() < 31) {
+      $('#topnav').removeClass('navbar-fixed-top');
+    }
   });
+});
